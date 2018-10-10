40239e4d9ec0e6269d0923682827adf51eb14678
Student <student@imac-14.local>
2018-10-10 16:27:37 +0200

    Pierwszy commit

---
+++ AdamskaPrognozaPogody/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AdamskaPrognozaPogody
+//
+//  Created by Student on 10.10.2018.
+//  Copyright © 2018 ITIK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+}
+
